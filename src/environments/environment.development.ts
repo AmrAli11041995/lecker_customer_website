80ba56b5d3f9c34d21d4338f89bde6cecf25c8ca
eng-Mustafa-Muhammed-Abdel-Kader <engmustafakader1995@gmail.com>
2026-08-24 22:26:33 +0300

    commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com/',
+  apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com',
   //apiBaseUrl: 'https://localhost:44359',
   apiBaseUrlGateWay: 'http://engmustafakader-001-site3.ktempurl.com/Gateway',
   //apiBaseUrlGateWay: 'https://localhost:44359/Gateway',
