26612a255c4e983e67addccecb825bbbf08267fa
eng-Mustafa-Muhammed-Abdel-Kader <engmustafakader1995@gmail.com>
2026-08-24 22:23:45 +0300

    commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com/',
-  apiBaseUrl: 'https://localhost:44359',
-  // apiBaseUrlGateWay: 'http://engmustafakader-001-site3.ktempurl.com/Gateway',
-  apiBaseUrlGateWay: 'https://localhost:44359/Gateway',
+  apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com/',
+  //apiBaseUrl: 'https://localhost:44359',
+  apiBaseUrlGateWay: 'http://engmustafakader-001-site3.ktempurl.com/Gateway',
+  //apiBaseUrlGateWay: 'https://localhost:44359/Gateway',
   fileBaseURL: 'http://engmustafakader-001-site5.ktempurl.com',
 
   debug: true
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  // apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com',
-  // apiBaseUrlGateWay: 'http://engmustafakader-001-site3.ktempurl.com/Gateway',
-  apiBaseUrl: 'https://localhost:44359',
-  apiBaseUrlGateWay: 'https://localhost:44359/Gateway',
+  apiBaseUrl: 'http://engmustafakader-001-site3.ktempurl.com',
+  apiBaseUrlGateWay: 'http://engmustafakader-001-site3.ktempurl.com/Gateway',
+  //apiBaseUrl: 'https://localhost:44359',
+  //apiBaseUrlGateWay: 'https://localhost:44359/Gateway',
   fileBaseURL: 'http://engmustafakader-001-site5.ktempurl.com',
 
   debug: false
